93f842b1fd5e975244393b728ca1e50dd6335b2e
ouzhuohong <smileuwu97@gmail.com>
2023-11-12 14:19:20 +0800

    add test

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,34 +1,109 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
-
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+contract Lock is IERC721Receiver {
+    struct LockedNFT {
+        address owner;
+        address targetCollection;
+        address collection;
+        uint256 tokenId;
+        uint256 lockTimestamp;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    mapping(address => mapping(uint256 => LockedNFT)) private _lockedNFTs;
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    event NFTLocked(
+        address indexed owner,
+        uint256[] tokenIds,
+        uint256 lockTimestamp
+    );
+    event NFTUnlocked(address indexed owner, uint256[] tokenIds);
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+    constructor() {}
 
-        owner.transfer(address(this).balance);
+    function lockNFT(
+        address targetCollection,
+        address collection,
+        uint256[] calldata tokenIds
+    ) external {
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            IERC721(collection).safeTransferFrom(
+                msg.sender,
+                address(this),
+                tokenIds[i]
+            );
+            _lockedNFTs[collection][tokenIds[i]] = LockedNFT({
+                owner: msg.sender,
+                targetCollection: targetCollection,
+                collection: collection,
+                tokenId: tokenIds[i],
+                lockTimestamp: block.timestamp
+            });
+        }
+        emit NFTLocked(msg.sender, tokenIds, block.timestamp);
+    }
+
+    function unlockNFT(
+        address collection,
+        uint256[] calldata tokenIds
+    ) external {
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            require(
+                _lockedNFTs[collection][tokenIds[i]].owner == msg.sender,
+                "Only owner can unlock"
+            );
+            IERC721(collection).safeTransferFrom(
+                address(this),
+                msg.sender,
+                tokenIds[i]
+            );
+            delete _lockedNFTs[collection][tokenIds[i]];
+        }
+        emit NFTUnlocked(msg.sender, tokenIds);
+    }
+
+    function releaseNFT(
+        address collection,
+        uint256[] memory tokenIds,
+        uint256 requiredLockDuration
+    ) external {
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            LockedNFT memory lockedNFT = _lockedNFTs[collection][tokenIds[i]];
+            require(
+                lockedNFT.targetCollection == msg.sender,
+                "Only target collection can release"
+            );
+            require(
+                lockedNFT.lockTimestamp + requiredLockDuration <=
+                    block.timestamp,
+                "Lock duration not met"
+            );
+            IERC721(collection).safeTransferFrom(
+                address(this),
+                lockedNFT.owner,
+                tokenIds[i]
+            );
+            _lockedNFTs[collection][tokenIds[i]]
+                .lockTimestamp += requiredLockDuration;
+        }
+    }
+
+    function getLockedNFT(
+        address collection,
+        uint256 tokenId
+    ) external view returns (LockedNFT memory) {
+        return _lockedNFTs[collection][tokenId];
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) external pure override returns (bytes4) {
+        return this.onERC721Received.selector;
     }
 }
---
+++ contracts/TestToken.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./ILock.sol";
+
+contract TestToken is ERC721 {
+    constructor() ERC721("TestToken", "TT") {}
+
+    function mint(address to, uint256 tokenId) external {
+        _mint(to, tokenId);
+    }
+
+    function mint2(
+        ILock lockContract,
+        address parentCollection,
+        uint256[] calldata tokenIds,
+        uint256 requiredLockDuration,
+        uint256 mintTokenId
+    ) external {
+        lockContract.releaseNFT(
+            parentCollection,
+            tokenIds,
+            requiredLockDuration
+        );
+        _mint(msg.sender, mintTokenId);
+    }
+}
