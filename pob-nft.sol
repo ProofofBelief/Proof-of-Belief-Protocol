e984b162c15eb35c211d29f51d0af23e29a39cf3
ProofofBelief <150526042+ProofofBelief@users.noreply.github.com>
2023-11-12 11:20:18 +0800

    Add files via upload

--- pob-nft.sol
+++ pob-nft.sol
@@ -2,10 +2,15 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "./pob-lock.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./lock.sol";
 
 contract PoBNFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
     address private _lockContractAddress;
+    mapping(uint256 => string) private _tokenURIs;
 
     constructor(address lockContractAddress) ERC721("PoBNFT", "PB") {
         _lockContractAddress = lockContractAddress;
@@ -16,13 +21,18 @@ contract PoBNFT is ERC721 {
         require(_isNFTLockedLongEnough(lockedNFTId), "NFT not locked long enough");
 
         // Mint the new NFT
-        uint256 tokenId = totalSupply() + 1;
+        uint256 tokenId = _tokenIdCounter.current();
         _mint(msg.sender, tokenId);
         _setTokenURI(tokenId, content);
+        _tokenIdCounter.increment();
 
         // Unlock the previously locked NFT
         LockContract lockContract = LockContract(_lockContractAddress);
         lockContract.unlockNFT(lockedNFTId);
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory uri) internal {
+        _tokenURIs[tokenId] = uri;
     }
 
     function _isNFTLockedLongEnough(uint256 lockedNFTId) internal view returns (bool) {
@@ -31,5 +41,9 @@ contract PoBNFT is ERC721 {
 
         // Check if the current time is at least 10 minutes after the lockTimestamp
         return (block.timestamp >= lockTimestamp + 600);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return _tokenURIs[tokenId];
     }
 }
