c6e460867b1ae46dd929cb062dfdf29ff1d383b8
ouzhuohong <smileuwu97@gmail.com>
2023-11-12 13:20:05 +0800

    add ILock.sol

---
+++ contracts/ILock.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface ILock {
+    struct LockedNFT {
+        address owner;
+        address targetCollection;
+        address collection;
+        uint256 tokenId;
+        uint256 lockTimestamp;
+    }
+
+    event NFTLocked(
+        address indexed owner,
+        uint256[] tokenIds,
+        uint256 lockTimestamp,
+    );
+    event NFTUnlocked(address indexed owner, uint256[] tokenIds);
+
+    function lockNFT(
+        address targetCollection,
+        address collection,
+        uint256[] memory tokenIds
+    ) external;
+
+    function unlockNFT(address collection, uint256[] tokenIds) external;
+
+    function releaseNFT(address collection, uint256[] tokenIds, uint256 requiredLockDuration) external;
+
+    function getLockedNFT(address collection,uint256 tokenId) external view returns (LockedNFT memory);
+}
